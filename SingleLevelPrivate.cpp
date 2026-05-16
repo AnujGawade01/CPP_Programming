55cfc899d00578b888fe5475a34437c7873aa157
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-05-16 15:59:34 +0530

    Update SingleLevelPrivate.cpp

--- SingleLevelPrivate.cpp
+++ SingleLevelPrivate.cpp
@@ -1,4 +1,4 @@
-// Here we have displayed single level inheritance with keping base class variables as private which will give error when called from main
+// Here we have displayed single level inheritance with keeping base class variables as private which will give error when called from main
 #include<iostream>
 using namespace std;
 
